b5fb22f4812353afc12cce9a2f232e045aa075de
Harry Wilson <harry.wilson1995a@gmail.com>
2020-02-21 20:29:46 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors 
   JOIN series 
-  ON series.author_id = author.id 
+  ON series.author_id = authors.id 
   JOIN subgenres 
   ON series.subgenres_id = subgenres.id;"
 end
