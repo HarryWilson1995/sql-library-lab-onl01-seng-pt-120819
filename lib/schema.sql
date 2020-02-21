b3531b73a2df088552f28aecf40fcf4024cb53ff
Harry Wilson <harry.wilson1995a@gmail.com>
2020-02-21 19:59:56 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -17,5 +17,12 @@ CREATE TABLE books (
     id INTEGER PRIMARY KEY,
     title TEXT,
     author_id INTEGER,
-    sub_genre_id
-  )
+    subgenre_id TEXT
+  );
+  
+  CREATE TABLE authors (
+    id INTEGER PRIMARY KEY, 
+    name TEXT
+  );
+  
+  CREATE TABLE sub
