8db1ead801a4ef9711893bee654303366cb87edc
Harry Wilson <harry.wilson1995a@gmail.com>
2020-02-21 20:26:53 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,9 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name, 
   FROM authors 
   JOIN series 
-  ON "
+  ON series.author_id = author.id 
+  JOIN subgenres 
+  ON series.subgenres_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
